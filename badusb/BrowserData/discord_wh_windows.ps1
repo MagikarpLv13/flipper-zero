df839c09ef82e3cbbd4ccc6b942228e20de58e4f
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-25 20:58:30 +0100

    Remove comment in ps1 test

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,10 +1,7 @@
-# Ouvre le Bloc-notes
 Start-Process notepad.exe -Wait
 
-# Pause pour laisser le Bloc-notes s'ouvrir complètement
 Start-Sleep -Seconds 2
 
-# Écrit "salut" dans le Bloc-notes
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait('salut')
 

